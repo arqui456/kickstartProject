67bd3c5d2dffb5c7545a91f6526e5386994d393e
arqui456 <arquimedes9137@hotmail.com>
2018-05-18 09:33:56 -0300

    Added User

--- src/jUnitTest/KickstartTests.java
+++ src/jUnitTest/KickstartTests.java
@@ -8,41 +8,26 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
 import entities.User;
 import entities.UserImplementation;
 
+
 class KickstartTests {
 
-	@BeforeAll
-    static void initAll() {
-    }
-
-    @BeforeEach
-    void init() {
-    }
-
     @Test
-    void succeedingTest() {
+    void UserTest() {
+    	
+    	User user = new UserImplementation("arqui456","123456","Arquimedes Vinicius Pereira de Franca Moura");
+    	
+    	// User login test
+    	assertEquals("arqui456", user.getLogin(), "Should be arqui456");
+    	
+    	// User password test
+    	assertEquals("123456", user.getPassword(), "Should be 123456");
+    	
+    	// User name test
+    	assertEquals("Arquimedes Vinicius Pereira de Franca Moura", user.getName(), "Should be Arquimedes Vinicius Pereira de Franca Moura");
+    	
+    	
     }
-
-    @Test
-    void failingTest() {
-        fail("a failing test");
-    }
-
-    @Test
-    @Disabled("for demonstration purposes")
-    void skippedTest() {
-        // not executed
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @AfterAll
-    static void tearDownAll() {
-    }
-
 }
